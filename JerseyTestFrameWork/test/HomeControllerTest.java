53995e898db736c637758b835e7ed82ea3ef2420
kiran-kumar-veigandla <kiran-kumar.veigandla@hpe.com>
2016-11-30 12:14:56 +0530

    Initial Commit

---
+++ JerseyTestFrameWork/test/HomeControllerTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+import org.glassfish.jersey.server.ResourceConfig;
+
+
+public class HomeControllerTest extends JerseyTest{
+
+	 @Path("hello")
+	    public static class HelloResource {
+	        @GET
+	        public String getHello() {
+	            return "Hello World!";
+	        }
+	    }
+	 
+	    @Override
+	    public Application configure() {
+	       
+	        return new ResourceConfig(HelloResource.class);
+	    }
+	 
+	    @Test
+	    public void test() {
+	        final String hello = target("hello").request().get(String.class);
+	        assertEquals("Hello World!", hello);
+	    }
+	
+	
+}

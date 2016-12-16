d173ef74ef7dbc1dff64dc817b96639155c0b6b4
kiran-kumar-veigandla <kiran-kumar.veigandla@hpe.com>
2016-12-16 15:16:49 +0530

    new changes

--- JerseyTestFrameWork/src/HomeController.java
+++ JerseyTestFrameWork/src/HomeController.java
@@ -1,7 +1,17 @@
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 
 @Path("/home")
 public class HomeController {
 
+	 @GET
+		@Path("/api")
+		public Response getData(){
+	    	System.out.println("from api !!! ");
+	    	return Response.status(Status.OK).entity("Hellooo World !!!").build();
+		}
+	
 }
